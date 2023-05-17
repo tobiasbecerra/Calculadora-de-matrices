8ac40cbc04d3990e3f500c1dc15395c30260b0af
Tobias Becerra <tobbecerra@ubp.edu.ar>
2023-05-16 23:02:21 -0300

    Se creó una funcion para cargar matrices y se la llamo en el MAIN

--- Calculadora de matrices.cpp
+++ Calculadora de matrices.cpp
@@ -5,15 +5,35 @@
 #include <iostream>
 using namespace std;
 
+void cargar_matrices(int matriz[10][10], int segunda_matriz[10][10], int filas, int columnas) {
+	cout << "\nCargue su primer matriz: " << endl;
+	for (int i = 0; i < filas; i++) {
+		for (int j = 0; j < columnas; j++) {
+			cout << "Digite un numero [" << i + 1 << "][" << j + 1 << "]: ";
+			cin >> matriz[i][j];
+		}
+	}
+	cout << "\nCargue su segunda matriz: " << endl;
+	for (int i = 0; i < filas; i++) {
+		for (int j = 0; j < columnas; j++) {
+			cout << "Digite un numero [" << i + 1 << "][" << j + 1 << "]: ";
+			cin >> segunda_matriz[i][j];
+		}
+	}
+}
+
 int main() {
 
 	int matriz[10][10];
+	int segunda_matriz[10][10];
 	int filas, columnas;
 
 	cout << "Ingrese la cantidad de FILAS que va a tener su matriz: ";
 	cin >> filas;
 	cout << "Ingrese la cantidad de COLUMNAS que va a tener su matriz: ";
 	cin >> columnas;
+
+	cargar_matrices(matriz, segunda_matriz, filas, columnas);
 
 	return 0;
 }
