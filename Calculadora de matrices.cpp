ca7502ba3c6a14cfc13a17f53300667ecd7c0116
Tobias Becerra <tobbecerra@ubp.edu.ar>
2023-05-16 22:22:26 -0300

    Se crearon los parametros matriz, filas y columnas. Ademas, comenzamos con el desarrollo base de la calculadora

---
+++ Calculadora de matrices.cpp
@@ -0,0 +1,28 @@
+/*
+	Calculadora de matrices - Parcial 2 Introduccion a la programacion
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int matriz[10][10];
+	int filas, columnas;
+
+	cout << "Ingrese la cantidad de FILAS que va a tener su matriz: ";
+	cin >> filas;
+	cout << "Ingrese la cantidad de COLUMNAS que va a tener su matriz: ";
+	cin >> columnas;
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
