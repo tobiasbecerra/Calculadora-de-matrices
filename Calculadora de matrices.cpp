8c4ffbadd68b36d8843e4bc51ccd4e2555e57e34
Lab7-XX <Lab7-XX@LAB2-PC16>
2023-05-18 17:45:38 -0300

    Se crearon las variables "segunda_filas" y "segunda_columnas" para usar en la segunda matriz

--- Calculadora de matrices.cpp
+++ Calculadora de matrices.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 using namespace std;
 
-void cargar_matrices(int matriz[10][10], int segunda_matriz[10][10], int filas, int columnas) {
+void cargar_matrices(int matriz[10][10], int segunda_matriz[10][10], int filas, int columnas, int segunda_filas, int segunda_columnas) {
 	cout << "\nCargue su primer matriz: " << endl;
 	for (int i = 0; i < filas; i++) {
 		for (int j = 0; j < columnas; j++) {
@@ -14,15 +14,15 @@ void cargar_matrices(int matriz[10][10], int segunda_matriz[10][10], int filas, 
 		}
 	}
 	cout << "\nCargue su segunda matriz: " << endl;
-	for (int i = 0; i < filas; i++) {
-		for (int j = 0; j < columnas; j++) {
+	for (int i = 0; i < segunda_filas; i++) {
+		for (int j = 0; j < segunda_columnas; j++) {
 			cout << "Digite un numero [" << i + 1 << "][" << j + 1 << "]: ";
 			cin >> segunda_matriz[i][j];
 		}
 	}
 }
 
-void mostrar_matrices(int matriz[10][10], int segunda_matriz[10][10], int filas, int columnas) {
+void mostrar_matrices(int matriz[10][10], int segunda_matriz[10][10], int filas, int columnas, int segunda_filas, int segunda_columnas) {
 	cout << "\nSu primera matriz ingresada es: " << endl;
 	for (int i = 0; i < filas; i++) {
 		cout << "| ";
@@ -33,9 +33,9 @@ void mostrar_matrices(int matriz[10][10], int segunda_matriz[10][10], int filas,
 		cout << "\n";
 	}
 	cout << "\nSu segunda matriz ingresada es: " << endl;
-	for (int i = 0; i < filas; i++) {
+	for (int i = 0; i < segunda_filas; i++) {
 		cout << "| ";
-		for (int j = 0; j < columnas; j++) {
+		for (int j = 0; j < segunda_columnas; j++) {
 			cout << segunda_matriz[i][j] << " ";
 		}
 		cout << "|";
@@ -43,23 +43,30 @@ void mostrar_matrices(int matriz[10][10], int segunda_matriz[10][10], int filas,
 	}
 }
 
-void sumar_matrices(int matriz[10][10], int segunda_matriz[10][10], int filas, int columnas) {
+void sumar_matrices(int matriz[10][10], int segunda_matriz[10][10], int filas, int columnas, int segunda_filas, int segunda_columnas) {
 
 }
 
 int main() {
 
 	int matriz[10][10];
-	int segunda_matriz[10][10];
 	int filas, columnas;
+	int segunda_matriz[10][10];
+	int segunda_filas, segunda_columnas;
 
-	cout << "Ingrese la cantidad de FILAS que va a tener su matriz: ";
+	cout << "Ingrese la cantidad de FILAS y COLUMNAS que va a tener su primer matriz: " << endl;
+	cout << "FILAS: ";
 	cin >> filas;
-	cout << "Ingrese la cantidad de COLUMNAS que va a tener su matriz: ";
+	cout << "COLUMNAS: ";
 	cin >> columnas;
+	cout << "Ingrese la cantidad de FILAS y COLUMNAS que va a tener su segunda matriz: " << endl;
+	cout << "FILAS: ";
+	cin >> segunda_filas;
+	cout << "COLUMNAS: ";
+	cin >> segunda_columnas;
 
-	cargar_matrices(matriz, segunda_matriz, filas, columnas);
-	mostrar_matrices(matriz, segunda_matriz, filas, columnas);
+	cargar_matrices(matriz, segunda_matriz, filas, columnas, segunda_filas, segunda_columnas);
+	mostrar_matrices(matriz, segunda_matriz, filas, columnas, segunda_filas, segunda_columnas);
 
 	return 0;
 }
