b5d4953111be0ef98ec612f1b35e1c75918f72f4
Lab7-XX <Lab7-XX@LAB2-PC13>
2023-05-22 20:48:57 -0300

    .

--- Calculadora de matrices.cpp
+++ Calculadora de matrices.cpp
@@ -24,14 +24,6 @@ int main() {
 	cout << "columnas: ";
 	cin >> segunda_columnas;
 
-	/*cargar_matrices(matriz, segunda_matriz, filas, columnas, segunda_filas, segunda_columnas);
-	mostrar_matrices(matriz, segunda_matriz, filas, columnas, segunda_filas, segunda_columnas);
-
-	sumar_matrices(matriz, segunda_matriz,filas, columnas, segunda_filas, segunda_columnas);
-	restar_matrices(matriz, segunda_matriz, filas, columnas, segunda_filas, segunda_columnas);
-	escalar_matriz(matriz, filas, columnas);
-	escalar_segundamatriz(segunda_matriz, segunda_filas, segunda_columnas);*/
-
 	int opc;
 
 	do {
@@ -39,7 +31,7 @@ int main() {
 		cout << "\t1. Sumar matrices" << endl;
 		cout << "\t2. Restar matrices" << endl;
 		cout << "\t3. Multiplicar matrices por un escalar" << endl;
-		cout << "\t4. Encontrar matriz inversa de una matriz" << endl;
+		cout << "\t4. Encontrar matriz inversa de una matriz (hasta 3x3)" << endl;
 		cout << "\t5. Resolver sistema de ecuaciones 3x3" << endl;
 		cout << "\t6. Salir\n" << endl;
 		cout << "\tOpcion: ";
@@ -57,8 +49,10 @@ int main() {
 		case 3:
 			cargar_matrices(matriz, segunda_matriz, filas, columnas, segunda_filas, segunda_columnas);
 			escalar_matriz(matriz, filas, columnas);
+			escalar_segundamatriz(segunda_matriz, segunda_filas, segunda_columnas);
 			break;
 		case 4:
+			inversa(matriz, filas, columnas, segunda_filas, segunda_columnas);
 			break;
 		case 5:
 			break;
--- funciones.h
+++ funciones.h
@@ -126,3 +126,101 @@ void escalar_segundamatriz(int segunda_matriz[10][10], int segunda_filas, int se
 		cout << "\n";
 	}
 }
+
+void inversa(int matriz[10][10], int filas, int columnas, int segunda_filas, int segunda_columnas) {
+	float matrizcofactores[3][3];
+	float matrizadjunta[3][3];
+	int inversa[3][3];
+
+	cout << "\nCargue su matriz: " << endl;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << "Digite un numero [" << i + 1 << "][" << j + 1 << "]: ";
+			cin >> matriz[i][j];
+		}
+	}
+
+	//matriz de cofactores
+	matrizcofactores[0][0] = (matriz[1][1] * matriz[2][2]) - (matriz[1][2] * matriz[2][1]);
+	matrizcofactores[0][1] = -((matriz[1][0] * matriz[2][2]) - (matriz[1][2] * matriz[2][0]));
+	matrizcofactores[0][2] = (matriz[1][0] * matriz[2][1]) - (matriz[1][1] * matriz[2][0]);
+
+	matrizcofactores[1][0] = -((matriz[0][1] * matriz[2][2]) - (matriz[0][2] * matriz[2][1]));
+	matrizcofactores[1][1] = (matriz[0][0] * matriz[2][2]) - (matriz[0][2] * matriz[2][0]);
+	matrizcofactores[1][2] = -((matriz[0][0] * matriz[2][1]) - (matriz[0][1] * matriz[2][0]));
+
+	matrizcofactores[2][0] = (matriz[0][1] * matriz[1][2]) - (matriz[0][2] * matriz[1][1]);
+	matrizcofactores[2][1] = -((matriz[0][0] * matriz[1][2]) - (matriz[0][2] * matriz[1][0]));
+	matrizcofactores[2][2] = (matriz[0][0] * matriz[1][1]) - (matriz[0][1] * matriz[1][0]);
+
+
+	//matriz adjunta (es la transpuesta de la matriz cofactores)
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			matrizadjunta[i][j] = matrizcofactores[j][i];
+		}
+	}
+	cout << endl;
+
+	//determinante por sarrus
+
+	float determinante = 0;
+
+	float diagonal_principal = (matriz[0][0] * matriz[1][1] * matriz[2][2]) +
+		(matriz[1][0] * matriz[2][1] * matriz[0][2]) +
+		(matriz[2][0] * matriz[0][1] * matriz[1][2]);
+
+	float diagonal_secundaria = (matriz[0][2] * matriz[1][1] * matriz[2][0]) +
+		(matriz[1][2] * matriz[2][1] * matriz[0][0]) +
+		(matriz[2][2] * matriz[0][1] * matriz[1][0]);
+
+	determinante = diagonal_principal - diagonal_secundaria;
+
+	if (determinante < 0) {
+		determinante = determinante * (-1);
+	}
+
+
+	//transpuesta de la matriz adjunta
+	float matriztranspuesta[3][3];
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			matriztranspuesta[i][j] = matrizadjunta[j][i];
+		}
+	}
+
+	float matriz_inversa[3][3];
+
+	//mostrar matriz que ingreso el usuario
+	cout << "Matriz ingresada:" << endl;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << matriz[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	/*
+	* Calcular inversa si el determinante es distinto de cero, si el determinante es cero se muestra un mensaje
+	* indicando que la matriz no tiene inversa.
+	*/
+	if (determinante != 0) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				matriz_inversa[i][j] = matriztranspuesta[i][j] / -determinante;
+			}
+		}
+		cout << "Matriz Inversa" << endl;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				cout << matriz_inversa[j][i] << " ";
+			}
+			cout << "\n";
+		}
+	}
+	else {
+		cout << "\nLa matriz ingresada no tiene inversa. \n";
+	}
+
+}
